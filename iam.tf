5e3d99133e70dc1886392079d401c3f3595fab62
Simons Attipoe <attipoe.simons@gmail.com>
2020-12-11 11:12:50 -0500

    Corrections made

--- iam.tf
+++ iam.tf
@@ -112,7 +112,7 @@ resource "aws_iam_policy" "policy" {
 EOF
 }
 
-resource "aws_iam_policy_attachment" "policy-attach" {
+resource "aws_iam_role_policy_ttachment" "policy-attach" {
   role       = aws_iam_role.iam_role.name
   policy_arn = aws_iam_policy.iam_policy.arn
 }
